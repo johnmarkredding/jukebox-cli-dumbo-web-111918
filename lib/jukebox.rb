8b3fbbc6e9a86c642c9fa9269569c128a6f69e7c
John Mark Redding <johnmarkredding@gmail.com>
2018-11-16 18:37:52 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -9,4 +9,40 @@ songs = [
   "Harry Chapin - Cats in the Cradle",
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
+def play(songs_list)
 
+  if (true)
+    song = gets.chomp.to_i - 1
+    puts songs_list[song]
+  else
+    puts "Invalid input, please try again"
+end
+
+def help 
+  puts "Possible Commands: help, play, list, exit."
+end
+def list(songs)
+  puts songs
+end
+def exit
+  puts "Goodbye"
+end
+
+def run
+  help
+  puts "Please enter a command:"
+  command = gets.chomp
+  case command
+  when "help"
+    help
+  when "play"
+    puts "Please enter a song name or number:"
+    play(songs)
+  when "list"
+    list
+  when "exit"
+    exit
+  else
+    puts "Not a command. Type help for all commands."
+  end
+end
